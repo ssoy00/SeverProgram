63fe5d13e3b997f59f0c7dd2316aeb8811391966
KimSoYoung <ggomag3243@gmail.com>
2024-05-13 15:09:40 +0900

    test

---
+++ src/main/java/com/busanit501/samplejsp501/MyServlet2.java
@@ -0,0 +1,24 @@
+package com.busanit501.samplejsp501;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// 웹브라우저에 접근시, http://localhost:8080/my
+@WebServlet(name = "myServlet2" , urlPatterns = "/my2")
+public class MyServlet2 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "내가 만든 MyServlet 파일에서 답보내기 연습중." + "</h1>");
+        out.println("<h1>" + "내가 만든 MyServlet 파일에서 답보내기 연습중." + "</h1>");
+        out.println("</body></html>");
+    }
+}
